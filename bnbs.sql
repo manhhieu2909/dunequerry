ab6b5b5420cb0227c9d16fe7258937233e1ecf57
Manh Hieu <malaxa.1262@gmail.com>
2023-11-14 09:16:57 +0700

    feat: SQL dune bnbs

---
+++ bnbs.sql
@@ -0,0 +1,33 @@
+WITH ranked_transactions AS (
+  SELECT
+    block_time,
+    block_number,
+    FROM_UTF8(data) AS decode_data,
+    "from" AS creator,
+    to AS owner,
+    hash,
+    ROW_NUMBER() OVER (PARTITION BY FROM_UTF8(data) ORDER BY block_time ASC) as rn
+  FROM
+    "bnb"."transactions"
+  WHERE
+    success = TRUE
+    AND value = 0
+    AND block_time >= TIMESTAMP '2023-06-18'
+    AND LENGTH(data) > 0
+    AND BYTEARRAY_STARTS_WITH(data, 0x646174613a2c7b2270223a226265702d3230222c226f70223a226d696e74222c227469636b223a22626e6273222c226964223a)
+)
+SELECT
+  '推特关注傻哥@bitfool1' AS promotion,
+  block_time,
+  block_number,
+  decode_data,
+  creator,
+  owner,
+  hash
+FROM
+  ranked_transactions
+WHERE
+  rn = 1
+ORDER BY
+  block_time ASC
+LIMIT 210000
